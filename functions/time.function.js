f2a4a922bcf8eeb35aada7871c327c5077dbfac6
Eric Boschmann <aesra.blight@gmail.com>
2022-11-29 13:17:39 +0100

    refactor: simplify time

--- functions/time.function.js
+++ functions/time.function.js
@@ -2,6 +2,7 @@ export default {
     name: 'time',
     description: 'show the current time and date',
     execute(os) {
-        os.next(new Date());
+        let date = new Date();
+        os.next(date.getHours()+":"+date.getMinutes()+" "+date.getDate()+"."+(date.getMonth()+1));
     }
 };
