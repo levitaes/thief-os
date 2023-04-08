c77b05a52430a747b91759ecaebe5e03cf48594d
Beniox <37512887+Beniox@users.noreply.github.com>
2023-04-08 12:52:45 +0100

    feat: fs add root folders

--- filesystem/FileSystem.js
+++ filesystem/FileSystem.js
@@ -9,10 +9,17 @@ class FileSystem {
         if (FileSystem.instance == null) {
             FileSystem.instance = this;
             this.root = new Directory("/", null);
+            new Directory("dev", this.root);
+            const home =new Directory("home", this.root);
+            new Directory("usr", this.root);
+            new Directory("bin", this.root);
+            new Directory("etc", this.root);
+            new File("README.md", home, "This is a README file.");
         }
         return FileSystem.instance;
     }
 }
+
 const fs = new FileSystem();
 window.fs = fs;
 
