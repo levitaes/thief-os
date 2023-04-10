5a16022a57f86e2f1cf3231a328287ae1ef93e5d
Beniox <37512887+Beniox@users.noreply.github.com>
2023-04-10 11:50:19 +0100

    feat: add serializer to save fs

--- filesystem/FileSystem.js
+++ filesystem/FileSystem.js
@@ -17,15 +17,96 @@ export class FileSystem {
         console.log("FileSystem constructor");
         if (FileSystem.instance == null) {
             FileSystem.instance = this;
-            this.root = new Directory("/", null);
-            new Directory("dev", this.root);
-            const home =new Directory("home", this.root);
-            new Directory("usr", this.root);
-            new Directory("bin", this.root);
-            new Directory("etc", this.root);
-            new File("README.md", home, "This is a README file.");
+
+            if (!this.restore()) {
+                console.log("FileSystem creation");
+                this.root = new Directory("/", null);
+                new Directory("dev", this.root);
+                const home = new Directory("home", this.root);
+                new Directory("usr", this.root);
+                new Directory("bin", this.root);
+                new Directory("etc", this.root);
+                new File("README.md", home, "This is a README file.");
+            }
         }
         return FileSystem.instance;
+    }
+
+    save() {
+        localStorage.setItem("FileSystem", JSON.stringify(this.serialize()));
+    }
+
+    restore() {
+        const json = localStorage.getItem("FileSystem");
+        if (json != null) {
+            const data = JSON.parse(json);
+            this.root = this.deserialize(data);
+            console.log(this.root);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Recursively serialize the file system tree as a JSON object.
+     * @returns {Object}
+     */
+    serialize() {
+        const data = {
+            type: "directory",
+            name: this.root.name,
+            children: [],
+        };
+        for (const child of this.root.dir.values()) {
+            data.children.push(this.serializeNode(child));
+        }
+        return data;
+    }
+
+    /**
+     * Recursively serialize an INode as a JSON object.
+     * @param {INode} node
+     * @returns {Object}
+     */
+    serializeNode(node) {
+        const data = {
+            type: node.constructor.name.toLowerCase(),
+            name: node.name,
+        };
+        if (node instanceof File) {
+            data.content = node.content;
+        } else if (node instanceof Directory) {
+            data.children = [];
+            for (const child of node.dir.values()) {
+                data.children.push(this.serializeNode(child));
+            }
+        } else if (node instanceof SysLink) {
+            data.target = node.target.name;
+        }
+        return data;
+    }
+
+    /**
+     * Recursively deserialize a JSON object as an INode.
+     * @param {Object} data
+     * @returns {INode}
+     */
+    deserialize(data) {
+        let node;
+        if (data.type === "file") {
+            node = new File(data.name, null, data.content);
+        } else if (data.type === "directory") {
+            node = new Directory(data.name, null);
+            for (const childData of data.children) {
+                const childNode = this.deserialize(childData);
+                childNode.parent = node;
+                node.dir.set(childNode.name, childNode);
+            }
+        } else if (data.type === "syslink") {
+            const targetNode = this.getNodeByPath(data.target);
+            node = new SysLink(data.name, null, targetNode);
+        }
+        return node;
     }
 }
 
@@ -78,9 +159,9 @@ export class WorkingDirectory {
             console.log(part);
             if (part === "..") {
                 this.goDirUp();
-            } else if(part === "") {
+            } else if (part === "") {
                 this.current = this.fs.root;
-            }else if (part !== "") {
+            } else if (part !== "") {
                 this.goDir(part);
             }
         }
