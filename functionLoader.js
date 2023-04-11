bb1f1c2d7fffb3e3ca305e89d64a5517deb73938
Beniox <37512887+Beniox@users.noreply.github.com>
2023-04-11 12:43:41 +0200

    chore: add comments

--- functionLoader.js
+++ functionLoader.js
@@ -1,4 +1,3 @@
-
 const os = {
     functions: new Map(),
     promiseResolve: null,
@@ -9,6 +8,10 @@ const os = {
     // fs: null,
 }
 
+/**
+ * Loads the functions into the os
+ * @returns {Promise<unknown>}
+ */
 os.load = () => {
     return new Promise(async (resolve, reject) => {
         import('./functions/functionList.js').then((module) => {
@@ -20,15 +23,21 @@ os.load = () => {
     });
 }
 
+/**
+ * Runs the command
+ * @param data {string} The command to run
+ */
 os.run = function (data) {
     const args = data.split(' ');
     const command = args.shift().toLowerCase();
 
+    // check if the command exists
     if (!this.functions.has(command)) {
         this.next('function doesnt exist');
         return;
     }
 
+    // check if the command has the correct amount of arguments
     if (this.functions.get(command).arguments !== -1) {
             
         if (args.length > this.functions.get(command).arguments) {
@@ -43,13 +52,14 @@ os.run = function (data) {
     }
 
     try {
+        // run the command
         this.functions.get(command).execute(this, args);
     }
     catch (error) {
-        say(error);
+        next(error);
     }
 
 }
 
-os.load();
+await os.load();
 export default os;
