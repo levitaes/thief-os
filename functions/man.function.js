3debed70956a76d2be5e9af2f6e1d54589cb88a7
Beniox <37512887+Beniox@users.noreply.github.com>
2023-04-20 14:57:07 +0200

    fix: man

--- functions/man.function.js
+++ functions/man.function.js
@@ -1,21 +1,23 @@
+import {AppManager} from "../appManager.js";
+
 export default {
     name: 'man',
     description: 'man [string] (display manual of function)',
     arguments: 1,
     execute(os, args) {
         const command = args.shift().toLowerCase();
-        if (!os.functions.has(command)) {
+        if (!AppManager.instance.apps.has(command)) {
             os.next('function not found');
             return;
         }
 
-        if(Array.isArray(os.functions.get(command).description)) {
+        if(Array.isArray(AppManager.instance.apps.get(command).description)) {
             let i;
-            for(i = 0; i < (os.functions.get(command).description.length -1); i++)
-                os.say(os.functions.get(command).description[i]);
-            os.next(os.functions.get(command).description[i]);
+            for(i = 0; i < (AppManager.instance.apps.get(command).description.length -1); i++)
+                os.say(AppManager.instance.apps.get(command).description[i]);
+            os.next(AppManager.instance.apps.get(command).description[i]);
         } 
         else
-            os.next(os.functions.get(command).description);
+            os.next(AppManager.instance.apps.get(command).description);
     }
 };
