cd18f7cc249d63939af3d7ef4c2bf477e8b62119
Beniox <37512887+Beniox@users.noreply.github.com>
2023-04-20 15:07:17 +0200

    feat: add download function to dialog

--- utils/dialog.js
+++ utils/dialog.js
@@ -56,6 +56,39 @@ export class Dialog {
         //TODO
         console.log(message);
     }
+
+    /**
+     * Download a file
+     * @param {string} data - The data to download
+     * @param {string} filename - The filename to download as
+     * @returns {Promise<void>}
+     */
+    static download(data, filename){
+        return new Promise((resolve, reject) => {
+            const element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+            resolve();
+        });
+
+    }
+
+    /**
+     * Upload a file
+     * @param {string} type - The file type to upload
+     * @returns {Promise<string>}
+     */
+    static upload(type){
+        //TODO
+    }
+
 }
 
 /**
