f4ad0d844d5420cc10bfd8fa33c89d623b7344ee
Eric Boschmann <e.boschmann@meddevo.com>
2023-10-06 16:44:30 +0200

    add: improve cotob ya know what it is

--- functions/cotob.function.js
+++ functions/cotob.function.js
@@ -1,21 +1,34 @@
 export default {
     name: 'cotob',
-    description: 'consigned to oblivion 龴ↀ◡ↀ龴',
+    description: 'consigned to oblivion',
     arguments: 0,
     clear: true,
     execute: async function (os) {
         os.dialog.clear();
+
+        //vars
+        let chapter;
         let response;
-        //stats
-        let hp = 3;
-        let dmg = 1;
+        let hp;
+        let dmg;
 
-        if (this.storage.get('chapter') === "")
+        //first time setup
+        if (this.storage.get('chapter') === undefined) {
             this.storage.set('chapter', 0);
+            chapter = this.storage.get('chapter');
+            this.storage.set('hp', 3);
+            this.storage.set('dmg', 1);
+        }
+        //get save state
+        else {
+            chapter = this.storage.get('chapter');
+            hp = this.storage.get('hp');
+            dmg = this.storage.get('dmg');
+        }
 
-        switch (this.storage.get('chapter')) {
+        switch (chapter) {
             case 0:
-                os.say(": Welcome Adventurer what is your name?")
+                os.say("Welcome Adventurer what is your name?")
                 response = await os.ask(">>")
                 let name = response.charAt(0).toUpperCase() + response.slice(1);
                 this.storage.set('name', name);
@@ -26,6 +39,7 @@ export default {
                 os.say(this.storage.get('name') + "? Is that really you?")
                 break;
             default:
+                os.say('cotob: unknown error');
                 break;
         }
         os.next();
