4924385d7c984fcaba1f02be5f9214be57f78af6
Eric Boschmann <e.boschmann@meddevo.com>
2023-10-06 15:53:40 +0200

    fix: add cuteness

--- terminal.js
+++ terminal.js
@@ -60,7 +60,7 @@ export class Terminal {
      * @returns {Promise<void>}
      */
     async init() {
-        Dialog.globalInstance.say("Welcome to the terminal");
+        Dialog.globalInstance.say("Welcome User 龴ↀ◡ↀ龴");
         this.loadHistory();
         await this.loop();
     }
