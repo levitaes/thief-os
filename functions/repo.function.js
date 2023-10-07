614753b3bbbfae0c5e6e3c527df886b6aba81d57
Beniox <37512887+Beniox@users.noreply.github.com>
2023-10-07 15:44:43 +0200

    add: recommended repo

--- functions/repo.function.js
+++ functions/repo.function.js
@@ -6,15 +6,33 @@ export default {
         'repo count (output available functions as integer)',
         'repo list (list available functions)',
         'repo install (install function)',
-        'repo remove (remove function)'], // arguments: 1,
+        "repo add (add an repository",
+        "repo re (add recommended repositories",
+        'repo remove (remove function)',
+        "repo purge (remove all data of function"], // arguments: 1,
     repositories: [["default", "/functions"], ["stable", "https://cdn.jsdelivr.net/gh/thief-hub/thief-os-stable/functions"]],
     async execute(os, args) {
+
+        const addRepo = async (url) => {
+            const tmp = await this.storage.get("repositories") ?? [];
+            for (const ele of tmp) {
+                if (ele[1] === args[1]) {
+                    os.dialog.next("repository already exists");
+                    return;
+                }
+            }
+            tmp.push([`repo${tmp.length}`, url]);
+            await this.storage.set("repositories", tmp);
+            os.logger.info(`added repository ${url}`);
+            os.dialog.say("repository added");
+        }
+
         switch (args[0]) {
             case "count":
                 os.next(AppManager.instance.apps.size);
                 break;
             case "list":
-                os.next(Array.from(AppManager.instance.apps.keys()));
+                os.next(Array.from(AppManager.instance.apps.keys()).join(", "));
                 break;
             case "install":
             case "i":
@@ -60,22 +78,12 @@ export default {
                     os.next("please specify a repository");
                     return;
                 }
-                if(!args[1].includes("https://")) {
+                if (!args[1].includes("https://")) {
                     os.next("please specify a valid url");
                     return;
                 }
 
-                const tmp = await this.storage.get("repositories") ?? [];
-                for (const ele of tmp) {
-                    if (ele[1] === args[1]) {
-                        os.next("repository already exists");
-                        return;
-                    }
-                }
-                tmp.push([`repo${tmp.length}`, args[1]]);
-                await this.storage.set("repositories", tmp);
-                os.logger.info(`added repository ${args[1]}`);
-                os.next("repository added");
+                await addRepo(args[1]);
                 break;
 
             case "purge":
@@ -90,9 +98,37 @@ export default {
                 }
                 break;
 
+            case "re":
+                if (RECOMMENDED_REPOS.length === 0) {
+                    os.dialog.say("No recommended Repos found")
+                    break;
+                }
+
+                for (const index in RECOMMENDED_REPOS) {
+                    const app = RECOMMENDED_REPOS[index];
+                    os.dialog.say(`${index}: ${app.name}, ${app.description}`);
+                }
+                const ans = Number(await os.dialog.ask("Which repo to add?"));
+                console.log(Number(ans));
+                if (!Number.isNaN(ans)) {
+                    console.log("add")
+                    await addRepo(RECOMMENDED_REPOS[ans].url);
+                }
+
+                break;
             default:
                 os.next("argument doesnt exist");
                 break;
         }
+
     },
 };
+
+
+const RECOMMENDED_REPOS = [
+    {
+        name: "Beniox",
+        description: "a few more or less useful tools",
+        url: "https://cdn.jsdelivr.net/gh/beniox/beniox-repository@latest/functions"
+    }
+]
