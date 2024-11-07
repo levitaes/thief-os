ff71976cde024237cbdf24a9f53ec3aa4d2b781f
Beniox <37512887+Beniox@users.noreply.github.com>
2024-11-07 19:19:45 +0100

    chore: rm detect folders

--- functions/rm.function.js
+++ functions/rm.function.js
@@ -1,11 +1,20 @@
+import {Directory} from "../filesystem/INode.js";
+
 export default {
     name: 'rm',
     description: 'remove (remove a file)',
     arguments: ["file"],
     execute(os, args) {
-        const path = args[0];
+        const path = args.at(-1);
+        console.log(path);
+        const arg = args.length > 1 ? args[0] : null;
         try {
-            os.fs.getNodeByPath(path).delete();
+            const target = os.wd.getFile(path);
+            if(target instanceof Directory && arg !== "-R") {
+                os.say(`rm: cannot remove '${path}': It is a directory`);
+                return;
+            }
+            target.delete();
         } catch (e) {
             os.say(`rm: cannot remove '${path}'`);
         }
